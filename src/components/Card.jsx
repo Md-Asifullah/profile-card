a283f7a9da23f3928df6a3d3bbadab8a15d49554
Asif <artisan.asif@gmail.com>
2025-02-24 09:44:58 +0800

    Added skill level

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,26 @@
-import Tag from './Tag';
+import Tag from "./Tag";
 
 export default function Card() {
-    return (
-        <div className="card">
-            <img src="asif.jpg" alt="Developer Image"></img>
-            <h2>Md Asifullah</h2>
-            <p>
-                I am a junior software engineer skilled in full-stack web
-                development, database management, and OOP. I’m passionate about
-                building responsive, interactive solutions using PHP, Laravel,
-                and Vue.js.
-            </p>
-            <div className="tags">
-                <Tag level="" name="HTML" type="code" />
-                <Tag level="" name="CSS" type="code" />
-                <Tag level="" name="JavaScript" type="code" />
-                <Tag level="" name="TypeScript" type="code" />
-                <Tag level="" name="PHP" type="code" />
-                <Tag level="" name="Vue" type="code" />
-                <Tag level="" name="Laravel" type="code" />
-                <Tag level="" name="MySQL" type="database" />
-                <Tag level="" name="WordPress" type="layer-group" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="card">
+      <img src="asif.jpg" alt="Developer Image"></img>
+      <h2>Md Asifullah</h2>
+      <p>
+        I am a junior software engineer skilled in full-stack web development,
+        database management, and OOP. I’m passionate about building responsive,
+        interactive solutions using PHP, Laravel, and Vue.js.
+      </p>
+      <div className="tags">
+        <Tag level="advanced" name="HTML" type="code" />
+        <Tag level="advanced" name="CSS" type="code" />
+        <Tag level="intermediate" name="JavaScript" type="code" />
+        <Tag level="begineer" name="TypeScript" type="code" />
+        <Tag level="intermediate" name="PHP" type="code" />
+        <Tag level="intermediate" name="Vue" type="code" />
+        <Tag level="intermediate" name="Laravel" type="code" />
+        <Tag level="intermediate" name="MySQL" type="database" />
+        <Tag level="intermediate" name="WordPress" type="layer-group" />
+      </div>
+    </div>
+  );
 }
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,45 +1,53 @@
-export default function Tag({ name, type = 'code' }) {
-    const bgColors = [
-        '#7b34bb',
-        '#7f848c',
-        '#5371b4',
-        '#653a77',
-        '#5334b8',
-        '#4736a7',
-        '#7085ae',
-        '#667da6',
-        '#7e70ab',
-        '#938c83',
-        '#507ca5',
-        '#8e9679',
-        '#715b7f',
-        '#8889c1',
-        '#7a54be',
-        '#4d60c4',
-        '#3566a1',
-        '#6b6b87',
-        '#547879',
-        '#865775',
-        '#405298',
-        '#327179',
-        '#955c7c',
-        '#7387a6',
-        '#704879',
-        '#633da1',
-        '#603672',
-        '#906389',
-        '#7990ad',
-        '#6c7dab',
-    ];
+export default function Tag({ name, type = "code", level }) {
+  const bgColors = [
+    "#7b34bb",
+    "#7f848c",
+    "#5371b4",
+    "#653a77",
+    "#5334b8",
+    "#4736a7",
+    "#7085ae",
+    "#667da6",
+    "#7e70ab",
+    "#938c83",
+    "#507ca5",
+    "#8e9679",
+    "#715b7f",
+    "#8889c1",
+    "#7a54be",
+    "#4d60c4",
+    "#3566a1",
+    "#6b6b87",
+    "#547879",
+    "#865775",
+    "#405298",
+    "#327179",
+    "#955c7c",
+    "#7387a6",
+    "#704879",
+    "#633da1",
+    "#603672",
+    "#906389",
+    "#7990ad",
+    "#6c7dab",
+  ];
 
-    const getRandomNumber = () => {
-        return Math.floor(Math.random() * 29);
-    };
-    // 💪 👶 👦
-    const randomColor = bgColors[getRandomNumber()];
-    return (
-        <div className="tag" style={{ backgroundColor: randomColor }}>
-            <i className={`fas fa-${type}`}></i> {name}
-        </div>
-    );
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 29);
+  };
+  const emoji = () => {
+    if (level === "advanced") {
+      return "💪";
+    } else if (level === "intermediate") {
+      return "👦";
+    } else {
+      return "👶";
+    }
+  };
+  const randomColor = bgColors[getRandomNumber()];
+  return (
+    <div className="tag" style={{ backgroundColor: randomColor }}>
+      <i className={`fas fa-${type}`}></i> {name} {emoji()}
+    </div>
+  );
 }
